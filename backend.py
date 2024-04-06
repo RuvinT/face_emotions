e110145030fcc82814c1a9046869922c62eb22c2
RuvinT <jagodaruvin@gmail.com>
2024-04-06 00:29:54 -0400

    test

--- backend.py
+++ backend.py
@@ -10,13 +10,18 @@ from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
-model = load_model("emotion_detection_model_final.h5")
+#model = load_model("emotion_detection_model_final.h5")
 
+@app.route('/')
+def home():
+    return 'flask app'
+    
+'''   
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     prediction = model.predict(data['image'])
     return jsonify(prediction.tolist())
-
+'''
 if __name__ == '__main__':
     app.run(debug=True)
