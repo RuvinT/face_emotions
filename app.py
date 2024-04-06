fde2bf75c3e45b10654260ea8b213ecc84e24b42
RuvinT <jagodaruvin@gmail.com>
2024-04-06 02:01:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 22:22:20 2024
 
+@author: ruvinjagoda
+"""
+
+from flask import Flask, request, jsonify
+
+# tensorflow.keras.models import load_model
+
+#from tensorflow.keras.models import load_model
+
+
+app = Flask(__name__)
+#model = load_model("emotion_detection_model_final.h5")
+
+@app.route('/')
+def home():
+    return 'flask app'
+    
+'''   
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict(data['image'])
+    return jsonify(prediction.tolist())
+'''
+if __name__ == '__main__':
+    app.run(debug=True)
